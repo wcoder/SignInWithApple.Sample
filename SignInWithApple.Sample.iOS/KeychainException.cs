41ba07e8cce4a4dfee2af1a03d6389e8a6d77a8f
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2020-10-28 17:22:29 +0300

    Fork xamarin.ios sample

---
+++ SignInWithApple.Sample.iOS/KeychainException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Security;
+
+namespace SignInWithApple.Sample.iOS
+{
+    public class KeychainException : Exception
+    {
+        public SecStatusCode StatusCode { get; private set; }
+
+        public KeychainException(SecStatusCode statusCode)
+        {
+            StatusCode = statusCode;
+        }
+    }
+
+    public class KeychainNoPasswordException : KeychainException
+    {
+        public KeychainNoPasswordException(SecStatusCode statusCode) : base(statusCode)
+        {
+        }
+    }
+
+    public class KeychainUnexpectedPasswordDataException : KeychainException
+    {
+        public KeychainUnexpectedPasswordDataException(SecStatusCode statusCode) : base(statusCode)
+        {
+        }
+    }
+
+    public class KeychainUnexpectedItemDataException : KeychainException
+    {
+        public KeychainUnexpectedItemDataException(SecStatusCode statusCode) : base(statusCode)
+        {
+        }
+    }
+
+    public class KeychainUnhandledErrorException : KeychainException
+    {
+        public KeychainUnhandledErrorException(SecStatusCode statusCode) : base(statusCode)
+        {
+        }
+    }
+}
---
+++ SignInWithApple.Sample.iOS/LoginViewController.designer.cs
@@ -0,0 +1,26 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+
+namespace SignInWithApple.Sample.iOS
+{
+    [Register("LoginViewController")]
+    partial class LoginViewController
+    {
+        [Outlet]
+        UIKit.UIStackView loginProviderStackView { get; set; }
+
+        void ReleaseDesignerOutlets()
+        {
+            if (loginProviderStackView != null)
+            {
+                loginProviderStackView.Dispose();
+                loginProviderStackView = null;
+            }
+        }
+    }
+}
